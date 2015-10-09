ed8332999664a828939383a26195fa18b6441c16
Jacob Townson <obewanjacobi@aim.com>
2015-10-10 00:09:42 +0100

    notes for future exploratory work

---
+++ scratch_work.R
@@ -0,0 +1,14 @@
+# business_data & checkin have 'type' & 'business id'
+
+# business & review have 'stars', 'type', 'business id'
+# checkin & review have 'business id' and 'type'
+
+# tip and review have 'user id' & 'text' & 'business id' & 'date' & 'type'
+# tip has a lot of things in common with others
+
+# user data seems to be the outcast, whereas the others are combined by business id among other things
+
+# type is defined as what category of data, ie.: 'business', 'checkin', 'review', etc.
+
+# business id's and user id's are consistent
+
